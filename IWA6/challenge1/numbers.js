28813c9cb309b2fa2db9456a0a3979ad919cd953
OntiretseMotene <ontiretse@codespace.co.za>
2023-04-12 17:33:40 +0200

    IWA6 challenge1 final version

--- IWA6/challenge1/numbers.js
+++ IWA6/challenge1/numbers.js
@@ -3,8 +3,15 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const primaryValid = typeof number == primaryPhone 
-const secondaryValid = typeof number == secondaryPhone
+/*const primaryValid = typeof number == primaryPhone 
+const secondaryValid = typeof number == secondaryPhone*/
+
+/* I changed "number" to the declared variables (primaryPhone and secondaryPhone) to fix the syntax errors.
+ * I also used the strict equality to validate that the phone numbers are strings as it is common to save them as a stings in JS.
+*/
+
+const primaryValid = typeof primaryPhone === 'string'
+const secondaryValid = typeof secondaryPhone === 'string'
 
 console.log('Primary phone is valid numerical string:', primaryValid)
-console.log('Secondary phone is valid numerical string:', secondaryValid )
+console.log('Secondary phone is valid numerical string:', secondaryValid)
