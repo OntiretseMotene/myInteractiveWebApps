217a92eeffc4bf2a40ca9f1c1f71252363334118
OntiretseMotene <ontiretse@codespace.co.za>
2023-04-12 19:20:49 +0200

    IWA6 challenge2 final version

---
+++ IWA6/challenge2/balance.js
@@ -0,0 +1,36 @@
+const rent = 400;
+//const tax = '8%';
+const tax = 0.08;
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00;
+const minuteOfDay = 00;
+
+// Only change below this line
+
+
+/*if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
+	const taxAsDecimal = tax / '100'
+  const startingAfterTax = salary * '1' - taxAsDecimal
+	const balace = starting - transport - food - rent
+}
+	
+console.log(balance.toFixed(3))*/
+
+//The corrected code
+
+/* I fixed the syntax errors.
+ * I changed "!== null" to "!== undefined" 
+ * I changed the equality operator to a strict equality for 'hourofDay' and 'minuteofDay' from "== '00'" to "=== 0"
+ * I fixed the spelling error in 'starting' to 'StartingafterTax'
+ * I formatted the final balance by adding the "R" currency and using the 'toFix(2) to round off to 2 decimal places
+*/
+
+if (hourOfDay !== undefined && minuteOfDay !== undefined && hourOfDay === 0 && minuteOfDay === 0) {
+    const taxAsDecimal = tax
+    const startingAfterTax = salary * (1 - taxAsDecimal);
+    const balance = startingAfterTax - transport - food - rent;
+
+    console.log(balance.toFixed(2))
+}
