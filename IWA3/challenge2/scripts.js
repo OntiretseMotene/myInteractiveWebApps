5e0052feba340c45156c2319897dbc91b7c40887
OntiretseMotene <ontiretse@codespace.co.za>
2023-04-14 20:54:43 +0200

    imported JS files

--- IWA3/challenge2/scripts.js
+++ IWA3/challenge2/scripts.js
@@ -1,10 +1,10 @@
 // scripts.js
 
-/*The javaScript files are not imported here and they must be as follows:
+/*The javaScript files are not imported here and they must be as follows:*/
 
- *import {role as nwabisaRole} from './nwabisa.js';
- *import {role as johannesRole} from './johannes.js';
- *import {role as alexRole} from './alex.js';
- */
+ import {role as nwabisaRole} from './nwabisa.js';
+ import {role as johannesRole} from './johannes.js';
+ import {role as alexRole} from './alex.js';
+ 
 
 console.log('Roles:', nwabisa.role, johanes.role, alex.role);
