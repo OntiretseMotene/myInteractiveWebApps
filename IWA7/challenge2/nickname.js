7d4b8514aa519e777b7f1b11c27a671d12cb3982
OntiretseMotene <ontiretse@codespace.co.za>
2023-04-16 23:52:28 +0200

    backticks

--- IWA7/challenge2/nickname.js
+++ IWA7/challenge2/nickname.js
@@ -1,4 +1,4 @@
 const nickName = "Timmy"; // Camel casing
 const firstName = "Timothy"; //camel casing
 
-console.log("Good Morning ${nickName} || ${firstName}!");
+console.log(`Good Morning ${nickName} || ${firstName}!`);
