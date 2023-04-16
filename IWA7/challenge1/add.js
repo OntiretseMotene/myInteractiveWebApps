aa1489dc53ac6791390b267cc6b5b34420155ada
OntiretseMotene <ontiretse@codespace.co.za>
2023-04-16 22:56:25 +0200

    Edited the console
    Added a user const

--- IWA7/challenge1/add.js
+++ IWA7/challenge1/add.js
@@ -1,2 +1,7 @@
 const value = 3 // Change the value from a string to a number
 console.log(value + 4 + value)
+
+/*The code logged 343 becuase:
+* The values were corhesed into strings
+* the values were concatinated instead of added into a sum.
+*/
---
+++ IWA7/challenge2/nickname.js
@@ -0,0 +1,9 @@
+const nickName = "Timmy"; // Camel casing
+const firstName = "Timothy"; //camel casing
+
+//Add user
+
+const user = firstName || firstName
+
+
+console.log("Good Morning ${user}!")
