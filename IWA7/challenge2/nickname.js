dc9a823d6dd7a6d7cff1e3858ab8dee4e6bb0889
OntiretseMotene <ontiretse@codespace.co.za>
2023-04-16 23:50:28 +0200

    testing

--- IWA7/challenge2/nickname.js
+++ IWA7/challenge2/nickname.js
@@ -1,9 +1,4 @@
 const nickName = "Timmy"; // Camel casing
 const firstName = "Timothy"; //camel casing
 
-//Add user
-
-const user = nickName || firstName
-
-
-console.log('Good Morning ${user}!');
+console.log("Good Morning ${nickName} || ${firstName}!");
