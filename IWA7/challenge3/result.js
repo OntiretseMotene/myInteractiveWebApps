7914be0dbe969ab5e37555082ea0187e6db8ef37
OntiretseMotene <ontiretse@codespace.co.za>
2023-04-17 13:40:20 +0200

    Update result.js

--- IWA7/challenge3/result.js
+++ IWA7/challenge3/result.js
@@ -10,10 +10,15 @@ const divider = '----------------------------------'
 
 // Only change below this line
 
-const owed = parseInt('R' + leoBalance + sarahBalance)
-const leo = "{leoName} + {leoSurname} + \"Owed\" + \"R\" + {sarahBalance}"
-const sarah = "{leoName} + {surname} + \"Owed\" + \"R\" + {sarahBalance}"
-const total = "Total amount owed: "
-const result = leo + sarah + divider + divider + total + owed + divider
+//const owed = parseInt('R' + leoBalance + sarahBalance)
+const owed = (parseFloat (leoBalance) + parseFloat(sarahBalance)).toFixed(2);
+//const leo = "{leoName} + {leoSurname} + \"Owed\" + \"R\" + {sarahBalance}";
+const leo = `${leoName} ${leoSurname} Owed: R ${(-parseFloat(leoBalance)).toFixed(2)}`;
+//const sarah = "{leoName} + {surname} + \"Owed\" + \"R\" + {sarahBalance}";
+const sarah = `${sarahName} ${sarahSurname} Owed: R ${(-parseFloat (sarahBalance)).toFixed(2)}`;
+//const total = "Total amount owed: ";
+const total = `Total amount owed: R ${parseFloat(owed).toLocateString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})}`;
+//const result = leo + sarah + divider + divider + total + owed + divider
+const result = `\n${leo} \n${sarah}\n${divider}\n${total.padStart(44)}\n${dicider}`;
 
 console.log(result)
