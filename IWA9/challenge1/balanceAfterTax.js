2f14507c25d68052e2dd4e9b5ad866bb1eb91e69
OntiretseMotene <ontiretse@codespace.co.za>
2023-04-18 19:31:52 +0200

    IWA9

---
+++ IWA9/challenge1/balanceAfterTax.js
@@ -0,0 +1,43 @@
+// declare inital variables
+
+const salary = 4000;
+const lodging = 'apartment'
+const size = 'large'
+
+
+// Only change the syntax below (not the values or key names)
+
+const expenses = {      //expenses object with food  and transport properties
+    food: 51.7501,
+    transport:  10.2,
+}; //added the semicolon, semicolons are used to separate statements. 
+  
+const tax = {
+    734: '3%',
+    234: '20%',
+    913: '12%',
+    415: '38%',
+    502: '42%',
+};
+
+const rent = {
+    none: 0,
+    'small-room': 200,
+    'large-room': 300,
+    'small-apartment': 400,
+    'large-apartment': 800,
+    'small-house': 1200,
+    'large-house': 2400,
+};
+
+// You can change below however you want
+
+const taxAsDecimal = parseFloat(tax[913] / 100); 
+
+/** Calculate the tax rate as a decimal and 
+ * added the parseFloat method tax value and calculate it as a decimal
+ */
+const startingAfterTax = salary * (1 - taxAsDecimal); //Starting salary after tax is calculated here
+const type = lodging + '-' + size;  //I concatinated the variables to get the rental value
+const balance = expenses.transport - expenses.food - expenses.food - rent[type]; // Create a new variable balance from items fetched from the expense object using .notation and the string valuable
+console.log(balance); // a numenric value will be logged. 
