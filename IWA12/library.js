03dc6ff0100d6974e43f3f632f9e8cbf93bb7970
OntiretseMotene <ontiretse@codespace.co.za>
2023-05-03 21:12:17 +0200

    Update library.js

--- IWA12/library.js
+++ IWA12/library.js
@@ -70,7 +70,7 @@ const book1 = {
     status:  document.querySelector('#book1 .status'),
     reserve: document.querySelector('#book1 .reserve'),
     checkout: document.querySelector('#book1 .checkout'),
-    checkin: document.querySelector('#book1 .checkin'),
+    checkIn: document.querySelector('#book1 .checkIn'),
 
 };
 
@@ -80,7 +80,7 @@ const book2 = {
     status:  document.querySelector('#book2 .status'),
     reserve: document.querySelector('#book2 .reserve'),
     checkout: document.querySelector('#book2 .checkout'),
-    checkin: document.querySelector('#book2 .checkin'),
+    checkIn: document.querySelector('#book2 .checkIn'),
 };
 
 //Book 3 object
@@ -89,28 +89,33 @@ const book3 = {
     status: document.querySelector('#book3 .status'),
     reserve: document.querySelector('#book3 .reserve'),
     checkout: document.querySelector('#book3 .checkout'),
-    checkin: document.querySelector('#book3 .checkin'),
+    checkIn: document.querySelector('#book3 .checkIn'),
 };
 
 
+//
+
 book1.reserve = STATUS_MAP.overdue.canReserve ? book1.reserve.enabled = true: book1.reserve.disabled = true
-book1.checkout = STATUS_MAP.overdue.canCheckout ? book1.checkout.enabled = true: book1.checkout.disabled = true
-book1.checkin = STATUS_MAP.overdue.canCheckIn ? book1.checkin.enabled = true: book1.checkout.disabled = true
+book1.checkOut = STATUS_MAP.overdue.canCheckout ? book1.checkout.enabled = true: book1.checkout.disabled = true
+book1.checkIn = STATUS_MAP.overdue.canCheckIn ? book1.checkIn.enabled = true: book1.checkIn.disabled = true
 
 book1.status.style.color = ' ';
 book1.status.style.color = STATUS_MAP.overdue.color;
 
+//
+
 book2.reserve = STATUS_MAP.reserved.canReserve ? book2.reserve.enabled = true : book2.reserve.disabled = true
 book2.checkout = STATUS_MAP.reserved.canCheckout ? book2.checkout.enabled = true : book2.checkout.disabled = true
-book2.checkin = STATUS_MAP.reserved.canCheckIn ? book2.checkin.enabled = true : book2.checkin.disabled = true
+book2.checkIn = STATUS_MAP.reserved.canCheckIn ? book2.checkIn.enabled = true : book2.checkIn.disabled = true
 
 book2.status.style.color = ' ';
 book2.status.style.color = STATUS_MAP.reserved.color;
 
+//
 
 book3.reserve = STATUS_MAP.shelf.canReserve ? book3.reserve.enabled = true : book3.reserve.disabled = true
 book3.checkout = STATUS_MAP.shelf.canCheckout ? book3.checkout.enabled = true : book3.checkout.disabled = true
-book3.checkin = STATUS_MAP.shelf.canCheckIn ? book3.checkin.enabled = true : book3.checkin.disabled = true
+book3.checkIn = STATUS_MAP.shelf.canCheckIn ? book3.checkIn.enabled = true : book3.checkIn.disabled = true
 
 book3.status.style.color = ' ';
 book3.status.style.color = STATUS_MAP.shelf.color;
